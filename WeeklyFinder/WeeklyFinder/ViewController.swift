b641e51b4335a511be0923ccd2c679037ea4f5b0
nyuchosee <85153525+nyuchosee@users.noreply.github.com>
2021-09-16 01:03:22 +0300

    Add files via upload

---
+++ WeeklyFinder/WeeklyFinder/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Ru Nue on 02.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        
+    }
+}
+
